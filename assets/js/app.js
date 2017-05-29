03e9ee0a2738f9a03b5977d43d5a629fbb7231b4
Pitt009 <gmartinez28837@gmail.com>
2017-05-29 09:42:24 -0500

    Primer modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,4 @@
-var modal = document.getElementById("modal");
-var boton1 document.getElementById("boton1").addEventListener("click",mostrarModal1);
-
-function mostrarModal1 (){
-	modal.style.display "block";
-}
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
