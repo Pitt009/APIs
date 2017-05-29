67ebdc3efd3f0162326a2473ca01c960bcb5bec3
Pitt009 <gmartinez28837@gmail.com>
2017-05-28 21:30:23 -0500

    Falta todo

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+var modal = document.getElementById("modal");
+var boton1 document.getElementById("boton1").addEventListener("click",mostrarModal1);
+
+function mostrarModal1 (){
+	modal.style.display "block";
+}
